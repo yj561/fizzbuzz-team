762a6cb85026f25bd3772c6c5ed724c46fb96868
yj561 <choyj5971@gmail.com>
2023-05-01 18:57:10 +0900

    feat: Print buzz
    if i is multiple of 5, print 'buzz'

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,6 +2,8 @@ def do_fizzbuzz(num: int):
     for i in range (1, num+1):
         if i % 3 == 0:
             print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
         else:
             print(i)
 
