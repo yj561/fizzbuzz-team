60f52df697e361f99d455fe7a3553705dc650fd6
yj561 <choyj5971@gmail.com>
2023-05-01 18:55:46 +0900

    feat: Print fizz
    if i is mutiple of 3, print 'fizz'
    else print i

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,9 @@
 def do_fizzbuzz(num: int):
-    print(f'hello {num}')
+    for i in range (1, num+1):
+        if i % 3 == 0:
+            print('fizz')
+        else:
+            print(i)
 
 if __name__ == "__main__":
-    do_fizzbuzz(1)
+    do_fizzbuzz(16)
